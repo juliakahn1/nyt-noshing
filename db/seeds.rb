0d9f36436b8fa6eb9b62c3fc0c89f505c204e17a
Julia Kahn <juliakahn1@gmail.com>
2023-10-04 14:24:46 -0700

    Add seed data for Recipes

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,22 +9,58 @@
 ApplicationRecord.transaction do
     puts "Destroying tables..."
     User.destroy_all
+    Recipe.destroy_all
 
     puts "Resetting primary keys..."
     ApplicationRecord.connection.reset_pk_sequence!('users')
+    ApplicationRecord.connection.reset_pk_sequence!('recipes')
+
 
     puts "Creating users..."
     User.create!(
       email: 'julia@email.com',
       password: 'password'
     )
+    User.create!(
+      email: 'boolia@email.com',
+      password: 'password'
+    )
 
-    10.times do
-      User.create!({
-        email: Faker::Internet.unique.email,
-        password: 'password'
-      })
-    end
+    puts "Creating recipes..."
+    Recipe.create!(
+      name: 'Perfect Boiled Eggs',
+      author: 'J. Kenji López-Alt',
+      blurb: 'If your goal is perfectly smooth, blemish-free boiled eggs that jump out of their shells every single time, I’ve got bad news: No technique in the world can promise that level of perfection.
+        But armed with data from scientific tests done with more than 90 testers and more than 700 boiled eggs, this technique for boiled eggs — technically steamed, as they cook in just an inch of water —
+        will maximize your odds. Fresher eggs will take slightly longer to peel, but they should peel just as cleanly as older eggs. The eggs in this recipe should be cooked straight from the refrigerator;
+        reduce cooking times by 1 minute if using room-temperature eggs.',
+      ingredients: ['Up to 12 eggs'],
+      cook_time: 'Around 10 minutes',
+      preparation: ['Find a lidded saucepan large enough to allow your eggs to comfortably fit on the bottom in a single layer. Add 1 inch of water, cover and bring to a boil.',
+        'Gently lower eggs into the saucepan using a slotted spoon or a steamer basket. (It’s O.K. if the eggs are partly submerged on the bottom of the pot, or elevated on a steamer rack and not
+        submerged at all.) Cover pan and cook eggs, adjusting the burner to maintain a vigorous boil, 6 minutes for a warm liquid yolk and firm whites, 8½ minutes for a translucent, fudgy yolk or 11 minutes
+        for a yolk that is just barely firm all the way through.',
+        'Drain eggs, then peel and eat immediately, or transfer them to a plate and allow them to cool naturally before storing in the refrigerator for up to a week directly in their shell. (A small dot made
+        with a permanent marker on the top of each cooked egg will ensure you don’t mix them up with the raw eggs.) Do not shock them in an ice bath after cooking; this makes them more difficult to peel.'],
+      image_url: 'https://static01.nyt.com/images/2019/09/25/dining/23eggrex2/23eggrex2-master768.jpg?w=1280&q=75',
+      tags: ['breakfast']
+    )
+
+    Recipe.create!(
+      name: 'Classic Potato Latkes',
+      author: 'Melissa Clark',
+      blurb: 'This recipe is for a classic, unadorned latke; no kohlrabi or cumin here. Serve them hot and make more than you think you need. They go fast.',
+      ingredients: ['2 large Russet potatoes (about 1 pound), scrubbed and cut lengthwise into quarters', '1 large onion (8 ounces), peeled and cut into quarters', '2 large eggs', '1/2 cup all-purpose flour',
+        '2 teaspoons course kosher salt, plus more for sprinkling', '1 teaspoon baking powder', '1/2 teaspoon freshly ground black pepper', 'Safflour or other oil, for frying'],
+      cook_time: '45 minutes',
+      preparation: ['Using a food processor with a coarse grating disc, grate the potatoes and onion. Transfer the mixture to a clean dishtowel and squeeze and wring out as much of the liquid as possible.
+        Working quickly, transfer the mixture to a large bowl. Add the eggs, flour, salt, baking powder and pepper, and mix until the flour is absorbed.',
+        'In a medium heavy-bottomed pan over medium-high heat, pour in about ¼ inch of the oil. Once the oil is hot (a drop of batter placed in the pan should sizzle), use a heaping tablespoon to drop the batter
+        into the hot pan, cooking in batches. Use a spatula to flatten and shape the drops into discs. When the edges of the latkes are brown and crispy, about 5 minutes, flip. Cook until the second side is deeply
+        browned, about another 5 minutes. Transfer the latkes to a paper towel-lined plate to drain and sprinkle with salt while still warm. Repeat with the remaining batter.'],
+      image_url: 'https://static01.nyt.com/images/2021/12/13/dining/MC-Classic-Potato-Latkes/merlin_199179672_13ff5d80-3b33-46de-9904-0ccaabe97917-master768.jpg?w=1280&q=75',
+      tags: ['sides']
+    )
 
     puts "Done!"
   end
