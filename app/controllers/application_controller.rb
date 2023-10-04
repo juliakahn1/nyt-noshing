9c5a8d7fa16afe21c3b343e41bf3ccad1aef9917
Julia Kahn <juliakahn1@gmail.com>
2023-10-04 09:40:29 -0700

    Add Gemfile dependencies, session support, request/response key transformations

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::API
+    before_action :snake_case_params # transform from camelCase to snake_case
+
+    private
+
+    def snake_case_params
+        params.deep_transform_keys!(&:underscore)
+    end
 end
