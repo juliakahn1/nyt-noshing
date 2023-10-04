4a0bdde6c9035711a4406374b7cd1cf8a4ccbbc2
Julia Kahn <juliakahn1@gmail.com>
2023-10-04 11:40:48 -0700

    Complete user auth, error handling, CSRF tokens

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,26 @@
 class ApplicationController < ActionController::API
-    # include ActionController::RequestForgeryProtection
-    # protect_from_forgery with: :exception # CSRF
+    rescue_from StandardError, with: :unhandled_error
+    rescue_from ActionController::InvalidAuthenticityToken,
+        with: :invalid_authenticity_token
 
-    # before_action :attach_authenticity_token, :snake_case_params # transform from camelCase to snake_case
-    before_action :snake_case_params # transform from camelCase to snake_case
+    include ActionController::RequestForgeryProtection
+    protect_from_forgery with: :exception # CSRF
+
+    before_action :attach_authenticity_token, :snake_case_params # transform from camelCase to snake_case
+
+    def test
+        if params.has_key?(:login)
+          login!(User.first)
+        elsif params.has_key?(:logout)
+          logout!
+        end
+
+        if current_user
+          render json: { user: current_user.slice('id', 'email', 'session_token') }
+        else
+          render json: ['No current user']
+        end
+      end
 
     def current_user
         return nil unless session[:session_token]
@@ -42,11 +59,27 @@ class ApplicationController < ActionController::API
 
     private
 
-    # def attach_authenticity_token
-    #     headers['X-CSRF-Token'] = masked_authenticity_token(session)
-    # end
+    def attach_authenticity_token
+        headers['X-CSRF-Token'] = masked_authenticity_token(session)
+    end
 
     def snake_case_params
         params.deep_transform_keys!(&:underscore)
+    end
+
+    def invalid_authenticity_token # cleans up error response
+        render json: { message: 'Invalid authenticity token' }, status: :unprocessable_entity
+    end
+
+    def unhandled_error(error) # errors for non-authenticity token errors
+        if request.accepts.first.html?
+            raise error
+        else
+            @message = "#{error.class} - #{error.message}"
+            @stack = Rails::BacktraceCleaner.new.clean(error.backtrace)
+            render 'api/errors/internal_server_error', status: :internal_server_error
+
+            logger.error "\n#{@message}:\n\t#{@stack.join("\n\t")}\n"
+        end
     end
 end
