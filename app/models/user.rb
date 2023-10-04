fc6aea7ce64622daac58052c6107a6755795cad3
Julia Kahn <juliakahn1@gmail.com>
2023-10-04 10:14:20 -0700

    Complete user model, except from associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -32,15 +32,18 @@ class User < ApplicationRecord
         end
     end
 
-    def ensure_session_token
-        self.session_token ||= generate_session_token
-    end
-
     def reset_session_token!
         self.session_token = generate_session_token
         self.save!
         self.session_token
     end
+    
+    private
+
+    def ensure_session_token
+        self.session_token ||= generate_session_token
+    end
+
 
     def generate_session_token
         while true
