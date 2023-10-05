f5d897679e0ba11439e2875b19fc5fe709a538a1
Julia Kahn <juliakahn1@gmail.com>
2023-10-05 14:26:06 -0700

    Add store and Root providers/browser routers to src > index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
+import configureStore from './store';
+
+// var to access store
+const store = configureStore()
+
+// expose store to window if in production mode
+if (process.env.NODE_ENV !== 'production') {
+  window.store = store;
+}
+
+const Root = () => {
+  return(
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  )
+}
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Root />
   </React.StrictMode>,
   document.getElementById('root')
 );
--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,5 +1,5 @@
 import { createStore, combineReducers, applyMiddleware, compose } from 'redux'
-import { thunk } from 'redux-thunk'
+import thunk from 'redux-thunk'
 
 const rootReducer = combineReducers({
 
