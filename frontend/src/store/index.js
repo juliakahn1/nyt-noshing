d3143e970da333d0e8000b214901f11a4c71eb7a
Julia Kahn <juliakahn1@gmail.com>
2023-10-05 14:11:52 -0700

    Add index.js

---
+++ frontend/src/store/index.js
@@ -0,0 +1,24 @@
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux'
+import { thunk } from 'redux-thunk'
+
+const rootReducer = combineReducers({
+
+})
+
+// set to difference store enhancers depending on if in prod
+let enhancer;
+
+if (process.env.NODE_ENV === 'production') {
+    enhancer = applyMiddleware(thunk);
+} else {
+    const logger = require('redux-logger').default;
+    const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+    enhancer = composeEnhancers(applyMiddleware(thunk, logger));
+}
+
+const configureStore = (preloadedState) => {
+    return createStore(rootReducer, preloadedState, enhancer);
+};
+
+// to be used by index.js in src to attach Redux store to React application
+export default configureStore
