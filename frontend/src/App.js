d6ee37176cbe17280c6882cf7c7ce25c681bb447
Julia Kahn <juliakahn1@gmail.com>
2023-10-06 17:03:20 -0700

    Add signup form and css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import LoginFormPage from './components/LoginFormPage';
+import SignUpFormPage from './components/SignUpFormPage';
 
 const App = () => {
   return (
     <Switch>
       <Route path="/login">
         <LoginFormPage />
+      </Route>
+      <Route path="/signup">
+        <SignUpFormPage />
       </Route>
     </Switch>
   );
