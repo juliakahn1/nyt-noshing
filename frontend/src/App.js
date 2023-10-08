29477c5b6eaf42349c90d3f96cfc3215d33dab94
Julia Kahn <juliakahn1@gmail.com>
2023-10-07 18:32:42 -0700

    Add navigation bar, login/out buttons, create accont button, and icon, SANS GOOD STYLING

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,17 +2,21 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import LoginFormPage from './components/LoginFormPage';
 import SignUpFormPage from './components/SignUpFormPage';
+import Navigation from './components/Navigation';
 
 const App = () => {
   return (
-    <Switch>
-      <Route path="/login">
-        <LoginFormPage />
-      </Route>
-      <Route path="/signup">
-        <SignUpFormPage />
-      </Route>
-    </Switch>
+    <>
+      <Navigation />
+      <Switch>
+        <Route path="/login">
+          <LoginFormPage />
+        </Route>
+        <Route path="/signup">
+          <SignUpFormPage />
+        </Route>
+      </Switch>
+    </>
   );
 }
 
