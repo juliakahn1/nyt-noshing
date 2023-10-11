206d7207ea5cb68c5d75e87e393ded74866cf6b9
Julia Kahn <juliakahn1@gmail.com>
2023-10-11 12:20:56 -0700

    Begin formatting grid, title and image

---
+++ frontend/src/components/RecipeShowPage/RecipeShowHero.js
@@ -0,0 +1,20 @@
+import React from "react"
+
+const RecipeShowHero = ({ recipe }) => {
+    return(
+        <>
+            {/* <div className="recipe-show-hero-wrapper">
+                <div className="recipe-show-hero-content">
+                    <h2 className='recipe-show-hero-name'>{ recipe.name }</h2>
+                    <h3 className='recipe-show-hero-author'>By {recipe.author}</h3>
+                </div>
+                <div className="recipe-show-hero-image">
+                    <img className="recipe-show-hero-image-photo" src="https://static01.nyt.com/images/2019/09/25/dining/23eggrex2/23eggrex2-master768.jpg?w=1280&q=75"></img>
+                </div>
+
+            </div> */}
+        </>
+    )
+}
+
+export default RecipeShowHero
--- frontend/src/components/RecipeShowPage/index.js
+++ frontend/src/components/RecipeShowPage/index.js
@@ -1,13 +1,44 @@
-import React from 'react';
+import React, { useEffect }  from 'react';
 import { useParams } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import RecipeShowHero from "./RecipeShowHero.js"
+import { fetchRecipe } from '../../store/recipes.js';
+import "./RecipeShowPage.scss"
 
 const RecipeShowPage = () => {
-    const recipeId = useParams()
-    // if logged in, go to show page, else trigger log in module
-    return(
+    const { recipeId } = useParams()
+    const dispatch = useDispatch()
+    const recipe = useSelector(store => store.recipes[recipeId])
+
+    useEffect(() => {
+        dispatch(fetchRecipe(recipeId))
+    }, [dispatch])
+
+    return recipe ? (
         <>
+            {/* <RecipeShowHero recipe={recipe} /> */}
+            <div className="show-grid-container">
+                <div className="show-content">
+                    <div className="show-intro-header">
+                        <header className="show-recipe-name-author">
+                            <h1 className="show-recipe-name">{recipe.name}</h1>
+                            <h2 className="show-recipe-author">By {recipe.author}</h2>
+                        </header>
+                    </div>
+                    <div className="show-intro-image-wrapper">
+                        <div className="show-intro-recipe-image">
+                            <div className="show-intro-recipe-photo">
+                                <img className="show-intro-actual-phot" src="https://static01.nyt.com/images/2019/09/25/dining/23eggrex2/23eggrex2-master768.jpg?w=1280&q=75"></img>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="show-recipe-stats-wrapper">
+                        
+                    </div>
+                </div>
+            </div>
         </>
-    )
+    ) : (<></>)
 }
 
 export default RecipeShowPage
