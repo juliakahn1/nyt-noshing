54e070ffa74154a462c22d3b0bf5b45afee7990c
Julia Kahn <juliakahn1@gmail.com>
2023-10-14 12:39:26 -0700

    Add Recipe Box route and component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import { Route, Switch } from 'react-router-dom';
 import Navigation from './components/Navigation';
 import RecipesHome from './components/RecipesHome';
 import RecipeShowPage from './components/RecipeShowPage';
+import RecipeBox from "./components/RecipeBox"
 
 const App = () => {
   return (
@@ -10,6 +11,7 @@ const App = () => {
       <Navigation />
       <Switch>
         <Route path="/recipes/:recipeId" component={RecipeShowPage}/>
+        <Route path="/recipe-box" component={RecipeBox} />
         <Route path="/" component={RecipesHome} />
       </Switch>
     </>
