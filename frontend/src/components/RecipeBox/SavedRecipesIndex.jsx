2cce21171c8a07253b09d37b25f7df06a6c9e2e5
Julia Kahn <juliakahn1@gmail.com>
2023-10-15 14:24:49 -0700

    Fix show page margin formatting

--- frontend/src/components/RecipeBox/SavedRecipesIndex.jsx
+++ frontend/src/components/RecipeBox/SavedRecipesIndex.jsx
@@ -5,22 +5,26 @@ import RecipeBoxNav from "./RecipeBoxNav"
 const SavedRecipesIndex = ({ savedRecipes }) => {
     return(
         <>
-            <div className="saved-index-outer-container">
-                <div className="saved-index-inner-container">
-                    <div className="saved-index-overflow-wrapper">
-                        <div className="saved-index-header-wrapper">
-                            <div className="saved-index-header">
-                                <h2 className="saved-index-header-title">Saved Recipes</h2>
-                                <p className="saved-index-header-save-count">{savedRecipes.length} recipes</p>
+            <div className="recipe-box-flexbox">
+                <RecipeBoxNav />
+                <div className="saved-index-outer-container">
+                    <div className="saved-index-inner-container">
+                        <div className="saved-index-overflow-wrapper">
+                            <div className="saved-index-header-wrapper">
+                                <div className="saved-index-header">
+                                    <h2 className="saved-index-header-title">Saved Recipes</h2>
+                                    <p className="saved-index-header-save-count">{savedRecipes.length} recipes</p>
+                                </div>
                             </div>
-                        </div>
-                        <div className="saved-index-tiles-wrapper">
-                            {/* reuse tile element here  */}
+                            <div className="saved-index-tiles-wrapper">
+                                {/* reuse tile element here  */}
+                            </div>
                         </div>
                     </div>
                 </div>
+                <RecipeBoxNav />
+
             </div>
-            <RecipeBoxNav />
         </>
     )
 
