24e4657fb654f3ed7ede8d4a5225ffba2725467c
Julia Kahn <juliakahn1@gmail.com>
2023-10-16 10:56:08 -0700

    Add Saved Recipes and by category tabs to nav, no functionality

--- frontend/src/components/RecipeBox/RecipeBoxNav.jsx
+++ frontend/src/components/RecipeBox/RecipeBoxNav.jsx
@@ -1,9 +1,31 @@
 import "./RecipeBoxNav.scss"
+import { useSelector } from "react-redux"
 
 const RecipeBoxNav = () => {
+    const recipesArr = Object.values(useSelector(store => store.recipes))
+    const savedRecipes = Object.values(useSelector(store => store.savedRecipes)).reverse()
+    const recipeTags = (recipesArr.map(recipe => recipe.tags).flat())
+    const recipeCategories = removeDuplicates(recipeTags)
+
+    function removeDuplicates(arr) {
+        return arr.filter((item, index) => arr.indexOf(item) === index);
+    }
+
+
     return(<>
-        <div className="recipebox-nav-inner-wrapper">
-            <p>hello</p>
+        <div className="recipebox-nav-saved-recipes-tab">
+            <ul className="recipebox-nav-saved-recipes-ul">
+                <li>
+                    <a className="recipebox-nav-saved-recipes-a">
+                        <span className="recipebox-nav-saved-recipes-tab-icon"></span>
+                        <span className="recipebox-nav-saved-recipes-tab-text">Saved Recipes</span>
+                        <span className="recipebox-nav-saved-recipes-tab-count">{savedRecipes.length}</span>
+                    </a>
+                </li>
+            </ul>
+            <div className="recipebox-nav-category-container">
+                <h3 className="recipebox-nav-category-header">by category</h3>
+            </div>
         </div>
     </>)
 }
