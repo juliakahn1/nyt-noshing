76a507154bf14ae201f8e79605719b3a8850de31
Julia Kahn <juliakahn1@gmail.com>
2023-10-19 21:38:19 -0700

    Remove dead code and comments

--- app/views/api/users/show.json.jbuilder
+++ app/views/api/users/show.json.jbuilder
@@ -1,18 +1,3 @@
-# assumes @user instance variable has been defines, structures data that should be rendere for it
-
 json.user do
     json.extract! @user, :id, :email
 end
-
-# pull all of user's saved recipes
-
-# saved_recipes = @user.saved_recipes
-
-# json.saved_recipes do
-#     saved_recipes.each do |recipe|
-#         json.set! recipe.id do
-#             json.extract! recipe, :id, :user_id, :recipe_id, :cooked
-#         end
-#     end
-
-# end
--- frontend/src/components/RecipeShowPage/NoteItem.js
+++ frontend/src/components/RecipeShowPage/NoteItem.js
@@ -5,43 +5,42 @@ import EditNoteModal from "./EditNoteModal"
 import { useState } from "react"
 
 const NoteItem = ({ note, recipeId }) => {
-    const dispatch = useDispatch()
-    const [mountModal, setMountModal] = useState(false)
+  const dispatch = useDispatch()
+  const [mountModal, setMountModal] = useState(false)
 
-    let currentUser = useSelector(store => {
-        return store.session.user ? store.session.user.id : null
-    })
+  let currentUser = useSelector(store => {
+    return store.session.user ? store.session.user.id : null
+  })
 
-    let authorButtons
-    (note.userId === currentUser) ? authorButtons = (
-        <div className="show-note-item-buttons-wrapper">
-            <button className="show-note-item-button" onClick={(e) => setMountModal(true)}>Edit</button>
-            <button className="show-note-item-button" onClick={() => dispatch(deleteNote(note.id, recipeId))}>Delete</button>
+  let authorButtons
+  (note.userId === currentUser) ? authorButtons = (
+    <div className="show-note-item-buttons-wrapper">
+      <button className="show-note-item-button" onClick={(e) => setMountModal(true)}>Edit</button>
+      <button className="show-note-item-button" onClick={() => dispatch(deleteNote(note.id, recipeId))}>Delete</button>
+    </div>
+  ) : authorButtons = (<></>)
+
+  let modal
+  mountModal ? modal = (
+    <EditNoteModal note={note} setMountModal={setMountModal} />
+  ) : modal = (<></>)
+
+  return (
+    <>
+      {modal}
+      <div className="show-note-item-wrapper">
+        <header className="show-note-item-header">
+          <div className="show-note-item-name-timestamp">
+            <h3 className="show-note-item-name">{note.name}</h3>
+          </div>
+        </header>
+        <div className="show-note-item-body">
+          <p className="show-note-item-body">{note.body}</p>
         </div>
-    ) : authorButtons = (<></>)
-
-    let modal
-    mountModal ? modal = (
-        <EditNoteModal note={note} setMountModal={setMountModal} />
-    ) : modal = (<></>)
-
-    return(
-        <>
-            { modal }
-            <div className="show-note-item-wrapper">
-                <header className="show-note-item-header">
-                    <div className="show-note-item-name-timestamp">
-                        <h3 className="show-note-item-name">{note.name}</h3>
-                        {/* <time className="show-note-item-timestamp">Time placeholder</time> */}
-                    </div>
-                </header>
-                <div className="show-note-item-body">
-                    <p className="show-note-item-body">{note.body}</p>
-                </div>
-                {authorButtons}
-            </div>
-        </>
-    )
+        {authorButtons}
+      </div>
+    </>
+  )
 }
 
 export default NoteItem
