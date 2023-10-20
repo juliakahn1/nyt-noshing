4ecc131bdf52433d51a5af7e1fea925149320fa1
Julia Kahn <juliakahn1@gmail.com>
2023-10-19 19:21:27 -0700

    Finish display for when user has already submitted a star rating

--- frontend/src/components/RecipeShowPage/RatingStarRadios.js
+++ frontend/src/components/RecipeShowPage/RatingStarRadios.js
@@ -1,31 +1,59 @@
-const RatingStarRadios = () => {
-    return(
-        <>
-        <form className="show-recipe-your-rating-stars-form">
-            <div role="radiogroup" className="show-recipe-interactive-stars">
-                <input className="star-radio" id="star-1" name="rating" type="radio" value="1"></input>
-                <label className="your-rating-star"title="1-star" focusable="false">
-                    <svg width="30" height="30" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" focusable="false"><path d="M7.5.297 9.184 5.48h5.449l-4.408 3.203 1.683 5.182L7.5 10.662l-4.408 3.203 1.683-5.182L.367 5.48h5.45L7.5.297Z" fill="currentColor" stroke="currentColor" strokeWidth="0.5" strokeLinejoin="round"></path></svg>
-                </label>
-                <input className="star-radio" id="star-2" name="rating" type="radio" value="2"></input>
-                <label className="your-rating-star" title="2-star" focusable="false">
-                    <svg width="30" height="30" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" focusable="false"><path d="M7.5.297 9.184 5.48h5.449l-4.408 3.203 1.683 5.182L7.5 10.662l-4.408 3.203 1.683-5.182L.367 5.48h5.45L7.5.297Z" fill="currentColor" stroke="currentColor" strokeWidth="0.5" strokeLinejoin="round"></path></svg>
-                </label>
-                <input className="star-radio" id="star-3" name="rating" type="radio" value="3"></input>
-                <label className="your-rating-star" title="3-star" focusable="false">
-                    <svg width="30" height="30" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" focusable="false"><path d="M7.5.297 9.184 5.48h5.449l-4.408 3.203 1.683 5.182L7.5 10.662l-4.408 3.203 1.683-5.182L.367 5.48h5.45L7.5.297Z" fill="currentColor" stroke="currentColor" strokeWidth="0.5" strokeLinejoin="round"></path></svg>
-                </label>
-                <input className="star-radio" id="star-4" name="rating" type="radio" value="4"></input>
-                <label className="your-rating-star" title="4-star" focusable="false">
-                    <svg width="30" height="30" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" focusable="false"><path d="M7.5.297 9.184 5.48h5.449l-4.408 3.203 1.683 5.182L7.5 10.662l-4.408 3.203 1.683-5.182L.367 5.48h5.45L7.5.297Z" fill="currentColor" stroke="currentColor" strokeWidth="0.5" strokeLinejoin="round"></path></svg>
-                </label>
-                <input className="star-radio" id="star-5" name="rating" type="radio" value="5"></input>
-                <label className="your-rating-star" title="5-star" focusable="false">
-                    <svg width="30" height="30" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" focusable="false"><path d="M7.5.297 9.184 5.48h5.449l-4.408 3.203 1.683 5.182L7.5 10.662l-4.408 3.203 1.683-5.182L.367 5.48h5.45L7.5.297Z" fill="currentColor" stroke="currentColor" strokeWidth="0.5" strokeLinejoin="round"></path></svg>
-                </label>
-            </div>
-        </form></>
-    )
+const RatingStarRadios = ({recipe}) => {
+  let starDisplay;
+
+  recipe.currentUserRating ? starDisplay = ( // if user has reviewed recipe
+    <>
+      {[...Array(5)].map((star, index) => {
+        index += 1;
+        if (recipe.avgRating >= index) {
+          return (
+            <svg className="your-rating-star red" width="30" height="30" viewBox="0 0 15 15" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" focusable="false">
+              <path d="M7.5.297 9.184 5.48h5.449l-4.408 3.203 1.683 5.182L7.5 10.662l-4.408 3.203 1.683-5.182L.367 5.48h5.45L7.5.297Z" fill="currentColor" stroke="currentColor" strokeWidth="0.5" strokeLinejoin="round"></path>
+            </svg>
+          )
+        } else {
+          return (
+            <svg className="your-rating-star" width="30" height="30" viewBox="0 0 15 15" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" focusable="false">
+              <path d="M7.5.297 9.184 5.48h5.449l-4.408 3.203 1.683 5.182L7.5 10.662l-4.408 3.203 1.683-5.182L.367 5.48h5.45L7.5.297Z" fill="currentColor" stroke="currentColor" strokeWidth="0.5" strokeLinejoin="round"></path>
+            </svg>
+          )
+        }
+      })}
+    </>
+  ) : starDisplay = ( // if user has not reviewed recipe
+    <>
+      <p>hello world</p>
+    </>
+  )
+  return (
+    <>
+      <form className="show-recipe-your-rating-stars-form">
+        <div role="radiogroup" className="show-recipe-interactive-stars">
+          { starDisplay }
+          {/* <input className="star-radio" id="star-1" name="rating" type="radio" value="1"></input>
+          <label className="your-rating-star" title="1-star" focusable="false">
+            <svg width="30" height="30" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" focusable="false"><path d="M7.5.297 9.184 5.48h5.449l-4.408 3.203 1.683 5.182L7.5 10.662l-4.408 3.203 1.683-5.182L.367 5.48h5.45L7.5.297Z" fill="currentColor" stroke="currentColor" strokeWidth="0.5" strokeLinejoin="round"></path></svg>
+          </label>
+          <input className="star-radio" id="star-2" name="rating" type="radio" value="2"></input>
+          <label className="your-rating-star" title="2-star" focusable="false">
+            <svg width="30" height="30" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" focusable="false"><path d="M7.5.297 9.184 5.48h5.449l-4.408 3.203 1.683 5.182L7.5 10.662l-4.408 3.203 1.683-5.182L.367 5.48h5.45L7.5.297Z" fill="currentColor" stroke="currentColor" strokeWidth="0.5" strokeLinejoin="round"></path></svg>
+          </label>
+          <input className="star-radio" id="star-3" name="rating" type="radio" value="3"></input>
+          <label className="your-rating-star" title="3-star" focusable="false">
+            <svg width="30" height="30" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" focusable="false"><path d="M7.5.297 9.184 5.48h5.449l-4.408 3.203 1.683 5.182L7.5 10.662l-4.408 3.203 1.683-5.182L.367 5.48h5.45L7.5.297Z" fill="currentColor" stroke="currentColor" strokeWidth="0.5" strokeLinejoin="round"></path></svg>
+          </label>
+          <input className="star-radio" id="star-4" name="rating" type="radio" value="4"></input>
+          <label className="your-rating-star" title="4-star" focusable="false">
+            <svg width="30" height="30" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" focusable="false"><path d="M7.5.297 9.184 5.48h5.449l-4.408 3.203 1.683 5.182L7.5 10.662l-4.408 3.203 1.683-5.182L.367 5.48h5.45L7.5.297Z" fill="currentColor" stroke="currentColor" strokeWidth="0.5" strokeLinejoin="round"></path></svg>
+          </label>
+          <input className="star-radio" id="star-5" name="rating" type="radio" value="5"></input>
+          <label className="your-rating-star" title="5-star" focusable="false">
+            <svg width="30" height="30" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" focusable="false"><path d="M7.5.297 9.184 5.48h5.449l-4.408 3.203 1.683 5.182L7.5 10.662l-4.408 3.203 1.683-5.182L.367 5.48h5.45L7.5.297Z" fill="currentColor" stroke="currentColor" strokeWidth="0.5" strokeLinejoin="round"></path></svg>
+          </label> */}
+        </div>
+      </form>
+    </>
+  )
 }
 
 export default RatingStarRadios
