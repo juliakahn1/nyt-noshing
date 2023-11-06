3bc127137d3ed83cdc3c4bd39373b7677397364d
Julia Kahn <juliakahn1@gmail.com>
2023-11-06 14:00:00 -0800

    Remove BrowserRouter duplicate

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Switch, BrowserRouter } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import Navigation from './components/Navigation';
 import RecipesHome from './components/RecipesHome';
 import RecipeShowPage from './components/RecipeShowPage';
@@ -13,14 +13,12 @@ const App = () => {
     <>
       <Navigation />
       <ScrollToTop />
-      <BrowserRouter>
         <Switch>
           <Route path="/recipes/:recipeId" component={RecipeShowPage} />
           <Route path="/recipe-box" component={RecipeBox} />
           <Route path="/:query" component={SearchResults} />
           <Route path="/" component={RecipesHome} />
         </Switch>
-      </BrowserRouter>
       <Footer />
     </>
   );
